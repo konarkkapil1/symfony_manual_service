a8f83506859961adc3f32b24f24d660398fcbd74
konark kapil <konarkkapil@rocketmail.com>
2021-01-22 18:49:40 +0530

    Added caching in api fetching service

--- src/services/Fetcher.php
+++ src/services/Fetcher.php
@@ -5,17 +5,30 @@ namespace App\services;
 
 
 use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class Fetcher
 {
+
     private $defaultUrl;
     public function __construct($defaultUrl){
         $this->defaultUrl = $defaultUrl;
     }
 
     public function get(){
-        $result = file_get_contents($this->defaultUrl);
-        return json_decode($result);
+        $cache = new FilesystemAdapter();
+        $users = $cache->getItem("users");
+        if(!$users->isHit()){
+            $result = file_get_contents($this->defaultUrl);
+            $users->set($result);
+            $cache->save($users);
+            $final = $users->get();
+        }
+//        $result = file_get_contents($this->defaultUrl);
+//        $users->set(file_get_contents($this->defaultUrl));
+        $final = $users->get();
+
+        return json_decode($final);
     }
 
 }
